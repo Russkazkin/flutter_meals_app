2eec92f082ebb37fde4665a12e4e938532dad372
Russkazkin <ruslan@skazkin.su>
2021-01-04 08:47:33 +0300

    Output Category list in the Categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/categories/category.dart';
+import '../data/categories.dart';
+
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: [
-        
-      ],
+      children: DUMMY_CATEGORIES
+          .map((categoryData) => Category(
+                title: categoryData.title,
+                color: categoryData.color,
+              ))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
