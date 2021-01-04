f5ff684854d4c6dbd3deec45981808f31724b078
Russkazkin <ruslan@skazkin.su>
2021-01-04 08:54:23 +0300

    Register Categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,18 +6,23 @@ import '../data/categories.dart';
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map((categoryData) => Category(
-                title: categoryData.title,
-                color: categoryData.color,
-              ))
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((categoryData) => Category(
+                  title: categoryData.title,
+                  color: categoryData.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
