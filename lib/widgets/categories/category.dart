866b88e65b22d59a8703f29b3da73a319f37c639
Russkazkin <ruslan@skazkin.su>
2021-01-05 08:16:33 +0300

    Add screens navigation

--- lib/widgets/categories/category.dart
+++ lib/widgets/categories/category.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../screens/category_meals.dart';
 
 class Category extends StatelessWidget {
   final String title;
@@ -6,18 +7,29 @@ class Category extends StatelessWidget {
 
   Category({this.title, this.color});
 
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return CategoryMeals();
+    },),);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title, style: Theme.of(context).textTheme.headline6,),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.7), color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(3),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(title, style: Theme.of(context).textTheme.headline6,),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.7), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(3),
         ),
-        borderRadius: BorderRadius.circular(3),
       ),
     );
   }
