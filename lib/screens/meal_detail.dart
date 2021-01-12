59159662329409686774bce9b416b3e56eba0df3
Russkazkin <ruslan@skazkin.su>
2021-01-12 07:19:53 +0300

    Output title and image on detail screen

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../data/meals.dart';
+
 class MealDetail extends StatelessWidget {
   static const route = '/meal-detail';
 
   @override
   Widget build(BuildContext context) {
     final id = ModalRoute.of(context).settings.arguments as String;
-    return Scaffold(appBar: AppBar(title: Text('$id'),), body: Center(child: Text('Meal - $id!'),),);
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == id);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(selectedMeal.title),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
